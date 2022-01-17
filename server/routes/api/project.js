c6ea4488c42d58b1a578366d974377064dc46d79
Yun-Jehyeok <54862291+Yun-Jehyeok@users.noreply.github.com>
2022-01-17 12:24:10 +0900

    Update project.js

--- server/routes/api/project.js
+++ server/routes/api/project.js
@@ -42,6 +42,7 @@ router.get('/', async (req, res) => {
     const projectFindResult = await Project.find().populate({
       path: 'creator',
     });
+    
     const result = { projectFindResult };
     res.json(result);
   } catch (e) {
