7058bfc98d3964c596ff799cc6a8a1c8f7dfd958
Yun-Jehyeok <54862291+Yun-Jehyeok@users.noreply.github.com>
2022-01-17 12:20:11 +0900

    Update style.js

--- client/src/components/comment/style.js
+++ client/src/components/comment/style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Comment_Button = styled.button`
+export const Comment_Button = styled.div`
   width: 10%;
   height: 40px;
   background-color: #ffffff;
