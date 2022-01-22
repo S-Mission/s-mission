e526f9180fb63d48cc14652d88277e118bef0e27
dae <h33223012@naver.com>
2022-01-22 13:40:58 +0900

    0122update

--- client/src/components/comment/style.js
+++ client/src/components/comment/style.js
@@ -1,12 +1,6 @@
 import { Button } from 'antd';
 import styled from 'styled-components';
 
-<<<<<<< HEAD
-export const Comment_Button = styled.button`
-  width: 10%;
-  height: 40px;
-  background-color: #ffffff;
-=======
 export const CommentBox = styled.div`
   display: flex;
   justify-content: space-between;
@@ -15,5 +9,4 @@ export const CommentBox = styled.div`
 export const CommentButton = styled(Button)`
   height: 48px;
   margin-left: 4px;
->>>>>>> f3f2c4e59f6203101027c9c859f4a78e2fdc82a7
 `;
